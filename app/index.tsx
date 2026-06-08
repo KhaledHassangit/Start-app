67e73477a6392d7c47c6cd914d2c0ce791d183f5
Khaled Hassan <khaledhassan199919@gmail.com>
2026-06-08 20:52:48 +0300

    m

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,14 @@
 // @ts-ignore
 import { Text, View } from "react-native";
 import "../global.css";
- 
+ import { SafeAreaView } from "react-native-safe-area-context";
+
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-red">
-      <Text className="text-xl font-bold text-blue-500">
+    <SafeAreaView className="flex-1 items-center justify-center h-100 bg-red-500">
+      <Text className="text-xl font-bold text-red-500">
         Welcome to Nativewind!
       </Text>
-    </View>
+    </SafeAreaView>
   );
 }
